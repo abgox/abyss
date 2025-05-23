2d146c4375e261723fee7eceecd8d896de89fb1c
abgox <abgohxf@outlook.com>
2025-05-23 14:42:36 +0800

    update bin/generate-list.ps1

--- bin/generate-list.ps1
+++ bin/generate-list.ps1
@@ -14,7 +14,8 @@ foreach ($_ in $manifests) {
     $info += "[$app]($($json.homepage))"
 
     # version
-    $info += "[v$($json.version)](./bucket/$($_.Name))"
+    # $info += "[v$($json.version)](./bucket/$($_.Name))"
+    $info += '<a href="./bucket/' + $_.Name + '"><img src="https://img.shields.io/badge/dynamic/json?url=https%3A%2F%2Fraw.githubusercontent.com%2Fabgox%2Fabgox-bucket%2Frefs%2Fheads%2Fmain%2Fbucket%2F' + $_.Name + '&query=%24.version&prefix=v&label=%20" alt="version" /></a>'
 
     # persist
     $info += if ($json.persist) { '✔️' }else { '➖' }
