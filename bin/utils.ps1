f31712f3cb14de2caca96133956c4f4130068999
abgox <abgohxf@outlook.com>
2025-06-26 09:50:35 +0800

    update bin/utils.ps1

--- bin/utils.ps1
+++ bin/utils.ps1
@@ -1133,23 +1133,17 @@ function A-Get-InstallerInfoFromWinget {
         return
     }
 
-    $latestVersion = $versionList.Content |
+    $latestVersion = ""
+
+    $versionList.Content |
     ConvertFrom-Json |
     ForEach-Object { if ($_.Name -notmatch '^\.') { $_.Name } } |
-    Sort-Object -Property @{
-        Expression = {
-            # 提取第一个连续的数字版本号部分（如 "7.6.10"）
-            if ($_ -match '^(\d+(?:\.\d+)*)') {
-                $matchedVersion = $matches[1]
-                try { [version]$matchedVersion } catch { [version]::new(0, 0) }
-            }
-            else {
-                [version]::new(0, 0) # 无数字版本号的项排最后
-            }
+    ForEach-Object {
+        $compare = A-Compare-Version $_ $latestVersion
+        if ($compare -gt 0) {
+            $latestVersion = $_
         }
-        Descending = $true
-    } |
-    Select-Object -First 1
+    }
 
     $url = "https://raw.githubusercontent.com/microsoft/winget-pkgs/master/manifests/$rootDir/$PackagePath/$latestVersion/$PackageIdentifier.installer.yaml"
 
@@ -1212,6 +1206,65 @@ function A-Get-InstallerInfoFromWinget {
     Write-Host
 
     $installerInfo
+}
+
+function A-Compare-Version {
+    <#
+    .SYNOPSIS
+        比较两个版本号字符串的大小，支持多种格式混合排序。
+
+    .DESCRIPTION
+        比较两个版本号字符串的大小，并返回 1 / -1 / 0
+        1 表示 v1 大于 v2
+        -1 表示 v1 小于 v2
+        0 表示 v1 等于 v2
+
+    .PARAMETER v1
+        第一个版本号字符串。
+
+    .PARAMETER v2
+        第二个版本号字符串。
+    #>
+    param (
+        [string]$v1,
+        [string]$v2
+    )
+
+    # 将版本号拆分成数组，支持 . 和 - 作为分隔符
+    $parts1 = $v1 -split '[\.\-]'
+    $parts2 = $v2 -split '[\.\-]'
+
+    $maxLength = [Math]::Max($parts1.Length, $parts2.Length)
+
+    for ($i = 0; $i -lt $maxLength; $i++) {
+        $p1 = if ($i -lt $parts1.Length) { $parts1[$i] } else { '' }
+        $p2 = if ($i -lt $parts2.Length) { $parts2[$i] } else { '' }
+
+        # 尝试将部分转换为数字
+        $num1 = 0
+        $num2 = 0
+        $isNum1 = [int]::TryParse($p1, [ref]$num1)
+        $isNum2 = [int]::TryParse($p2, [ref]$num2)
+        if ($isNum1 -and $isNum2) {
+            if ($num1 -gt $num2) { return 1 }
+            elseif ($num1 -lt $num2) { return -1 }
+        }
+        elseif ($isNum1 -and !$isNum2) {
+            # 数字比字符串大
+            return 1
+        }
+        elseif (!$isNum1 -and $isNum2) {
+            return -1
+        }
+        else {
+            # 都是字符串，直接比较
+            $cmp = [string]::Compare($p1, $p2)
+            if ($cmp -ne 0) { return $cmp }
+        }
+    }
+
+    # 所有部分都相等
+    return 0
 }
 
 
