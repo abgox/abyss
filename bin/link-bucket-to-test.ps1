5313aba192edcd7920b75d3871460a880e116483
abgox <abgohxf@outlook.com>
2025-10-08 18:36:58 +0800

    (chore): add bin/link-bucket-to-test.ps1

---
+++ bin/link-bucket-to-test.ps1
@@ -0,0 +1,8 @@
+﻿$root_path = scoop config root_path
+
+if (-not $root_path) {
+    Write-Host "Scoop does not have a root_path configuration." -ForegroundColor Red
+    exit 1
+}
+
+New-Item -ItemType SymbolicLink -Path "$root_path\buckets\abyss-test" -Target "$PSScriptRoot\.." -Force
