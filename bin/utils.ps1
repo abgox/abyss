ea14451a8ac1883c3e237aa379b552600979ed28
abgox <abgohxf@outlook.com>
2025-12-08 20:40:32 +0800

    (chore): update utils.ps1

--- bin/utils.ps1
+++ bin/utils.ps1
@@ -1187,8 +1187,35 @@ function A-Get-UninstallEntryByAppName {
 
     return $null
 }
-function A-Get-VersionFromGithubApi {
-    if ($url -notlike 'https://github.com/*') {
+function A-Get-VersionFromGithubAPI {
+    if (-not $json) {
+        Write-Host "::error::`$json is invalid." -ForegroundColor Red
+        return
+    }
+
+    $url = if ($json.autoupdate.architecture.'64bit'.url) {
+        $json.autoupdate.architecture.'64bit'.url
+    }
+    elseif ($json.autoupdate.architecture.arm64.url) {
+        $json.autoupdate.architecture.arm64.url
+    }
+    elseif ($json.autoupdate.architecture.'32bit'.url) {
+        $json.autoupdate.architecture.'32bit'.url
+    }
+    else {
+        $json.autoupdate.url
+    }
+
+    if ($url -is [array]) {
+        $url = $url | Where-Object { $_ -like 'https://github.com/*/*' } | Select-Object -First 1
+    }
+
+    if (-not $url) {
+        Write-Host "::error::`$url is invalid." -ForegroundColor Red
+        return
+    }
+
+    if ($url -notlike 'https://github.com/*/*') {
         Write-Host "::error::'$url' is not a github url." -ForegroundColor Red
         return
     }
