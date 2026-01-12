3b1e8281b5f44fba4fdbc2a39f8425e829d2de6c
abgox <abgohxf@outlook.com>
2026-01-12 20:40:21 +0800

    (chore): add sort-json.ps1 and workflow

---
+++ script/sort-json.ps1
@@ -0,0 +1,117 @@
+﻿#Requires -Version 7.0
+
+$order = [ordered]@{
+    '##'           = ''
+    version        = ''
+    description    = ''
+    homepage       = ''
+    license        = [ordered]@{
+        identifier = ''
+        url        = ''
+    }
+    notes          = ''
+    'notes-cn'     = ''
+    depends        = ''
+    suggest        = ''
+    # url            = ''
+    # hash           = ''
+    architecture   = [ordered]@{
+        '64bit' = [ordered]@{
+            url          = ''
+            hash         = ''
+            extract_dir  = ''
+            extract_to   = ''
+            env_add_path = ''
+            bin          = ''
+            shortcuts    = ''
+            # pre_install  = ''
+            # installer    = ''
+            # post_install = ''
+            # uninstaller  = ''
+        }
+        'arm64' = [ordered]@{
+            url          = ''
+            hash         = ''
+            extract_dir  = ''
+            extract_to   = ''
+            env_add_path = ''
+            bin          = ''
+            shortcuts    = ''
+            # pre_install  = ''
+            # installer    = ''
+            # post_install = ''
+            # uninstaller  = ''
+        }
+    }
+    extract_dir    = ''
+    extract_to     = ''
+    env_set        = ''
+    env_add_path   = ''
+    innosetup      = ''
+    psmodule       = ''
+    fonts          = ''
+    commands       = ''
+    bin            = ''
+    shortcuts      = ''
+    persist        = ''
+    pre_install    = ''
+    installer      = ''
+    post_install   = ''
+    pre_uninstall  = ''
+    uninstaller    = ''
+    post_uninstall = ''
+    checkver       = ''
+    autoupdate     = ''
+}
+
+$root = Split-Path $PSScriptRoot -Parent
+
+function Sort-JsonByOrder {
+    param(
+        [Parameter(Mandatory)]
+        [System.Collections.IDictionary]$JsonObject,
+
+        [Parameter(Mandatory)]
+        [System.Collections.IDictionary]$Order
+    )
+
+    $result = [ordered]@{}
+
+    foreach ($key in $Order.Keys) {
+        if ($JsonObject.Contains($key)) {
+            $value = $JsonObject[$key]
+            $subOrder = $Order[$key]
+
+            if ($value -is [System.Collections.IDictionary] -and
+                $subOrder -is [System.Collections.IDictionary]) {
+
+                $result[$key] = Sort-JsonByOrder $value $subOrder
+            }
+            else {
+                $result[$key] = $value
+            }
+        }
+    }
+
+    foreach ($key in $JsonObject.Keys) {
+        if (-not $result.Contains($key)) {
+            $result[$key] = $JsonObject[$key]
+        }
+    }
+
+    return $result
+}
+
+Get-ChildItem "$root\bucket" -Recurse -File -Filter *.json | ForEach-Object {
+    $old = Get-Content $_.FullName -Raw
+    $json = $old | ConvertFrom-Json -AsHashtable
+
+    $sortedJson = Sort-JsonByOrder -JsonObject $json -Order $order
+    $new = $sortedJson | ConvertTo-Json -Depth 10
+
+    if ($new -ne $old) {
+        $new | Set-Content $_.FullName -Encoding UTF8
+    }
+}
+
+. "$root\bin\formatjson.ps1"
