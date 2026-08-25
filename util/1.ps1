bbeb822f4fb6392deed21a108e98ac493f707234
abgox <me@abgox.com>
2026-08-25 20:50:50 +0800

    (chore): improve util load logic

--- util/1.ps1
+++ util/1.ps1
@@ -1,3 +1,2 @@
-﻿'core', 'file', 'installer', 'lifecycle', 'scoop', 'system', 'var' | ForEach-Object {
-    . $PSScriptRoot\lib\$_.ps1
-}
+﻿'core', 'file', 'installer', 'lifecycle', 'scoop', 'system', 'var' | ForEach-Object { . $PSScriptRoot\lib\$_.ps1 }
+$abgox_abyss.version = 1
